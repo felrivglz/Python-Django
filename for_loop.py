c6dbdf343cafb9b19c3b639113b37accbea66ba0
FelipeRivera <felrivglz@gmail.com>
2017-02-24 13:41:57 -0600

    first commit

---
+++ for_loop.py
@@ -0,0 +1,18 @@
+prices = [2.5, 3.5, 4.5]
+"""
+'price' is a temporary vaariable that holds
+one of the 'prices' in the list for eache run
+"""
+total=0
+for price in prices :
+    print('Price is', price)
+    """
+    In python 2.7 'print 'Price is', price'.
+    because in the:
+        TIME IN THE LOOP   price      output
+        frist              2.5        Price is 2.5
+        second             3.5        Price is 3.5
+        third              4.5        Price is 4.5
+    """
+    total=total + price
+    print ('Total is:', total)
